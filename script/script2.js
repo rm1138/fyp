6da0c67ce01c05706bd0d860f8820c3b4752efe5
Ricky Lam <rm1138@gmail.com>
2015-03-18 02:51:17 +0800

    making noise

--- script/script2.js
+++ script/script2.js
@@ -1,3 +1,6 @@
+//head-start------------
+var wk = new Worker("script/worker.js");
+//head-end--------------
 "use strict";
 var AnimatedCanvas = AnimatedCanvas || function (canvasDomID) {
     "use strict"
@@ -65,6 +68,25 @@ var ac = new AnimatedCanvas("myCanvas");
 var anImg = new AnimatedImage("id", "img.jpg", (Math.random() * 1200), (Math.random() * 700));
 ac.add(anImg.getId(), anImg);
 
-var wk = new Worker("script/worker.js");
-wk.postMessage(ac,[ac]);
-var wk2 = new Worker("script/worker2.js");
+
+var source = document.getElementById("img");
+source.src = "img/bg.jpg"
+source.onload = function () {
+    var canvas = document.getElementById("myCanvas");
+    var tempContext = canvas.getContext("2d");
+    var canvasData = tempContext.createImageData(canvas.width, canvas.height);
+       // console.log(canvasData.data);
+    var buf;
+    setInterval(function(){
+        buf = canvasData.data.buffer;
+        if(buf.byteLength > 0)
+            wk.postMessage(buf, [buf]);
+    }, 1000/30);
+    wk.onmessage = function (e) {
+        var arr = new Uint8ClampedArray(e.data);
+        canvasData = tempContext.createImageData(canvas.width, canvas.height);
+        canvasData.data.set(arr);
+        tempContext.putImageData(canvasData, 0, 0);
+    }
+    //tempContext.clearRect(0, 0, canvas.width, canvas.height);
+}
--- script/worker.js
+++ script/worker.js
@@ -1,8 +1,18 @@
-var temp = setInterval(function(){
-    console.log("worker1");
-}, 500)
-
 onmessage = function(e){
-    clearInterval(temp);
-    console.log(e);
+    var data = new Uint32Array(e.data);
+
+    for (var y = 0; y < 1600; y++) {
+        for (var x = 0; x < 2560; x++) {
+            
+
+            data[y * 2560 + x] =
+                (255   << 24) |    // alpha
+                (Math.random() * 0xff << 16) |    // blue
+                (Math.random() * 0xff <<  8) |    // green
+                 Math.random() * 0xff;            // red
+            
+        }
+    }
+    self.postMessage(data.buffer, [data.buffer]);
 };
+
